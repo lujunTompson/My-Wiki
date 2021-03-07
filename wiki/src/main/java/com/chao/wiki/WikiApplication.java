16d4ba40b187f8d91f5faaad0de42e44c4bae4c9
Thompson Wang <m18916275937@163.com>
2021-03-07 14:51:57 +0800

    2-5 revise startpage

--- wiki/src/main/java/com/chao/wiki/WikiApplication.java
+++ wiki/src/main/java/com/chao/wiki/WikiApplication.java
@@ -1,13 +1,25 @@
 package com.chao.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class WikiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(WikiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+
+		logger.info("start successfully!");
+		logger.info("address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
+//		SpringApplication.run(WikiApplication.class, args);
 	}
 
 }
