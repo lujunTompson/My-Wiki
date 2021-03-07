ff3ec4e359f9b8deca4b59e50f16131e1c209c8a
Thompson Wang <m18916275937@163.com>
2021-03-07 15:17:30 +0800

    2-6 develop the api for "Hello world" the url: http://127.0.0.1:8880/hello

--- wiki/src/main/java/com/chao/wiki/WikiApplication.java
+++ wiki/src/main/java/com/chao/wiki/WikiApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
+@ComponentScan("com.chao")
 @SpringBootApplication
 public class WikiApplication {
 
