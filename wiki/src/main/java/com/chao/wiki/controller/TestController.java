6802fe27a41816f5af482fa75215f4c86de9a60d
Thompson Wang <m18916275937@163.com>
2021-03-07 15:51:28 +0800

    2-7 add hello test api -- test.http

--- wiki/src/main/java/com/chao/wiki/controller/TestController.java
+++ wiki/src/main/java/com/chao/wiki/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.chao.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -10,5 +13,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Post test, we hope the name is " + name;
     }
 }
