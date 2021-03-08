79a1b9c2f62a6ac9f097167acfaa48d76d3a43c8
Thompson Wang <m18916275937@163.com>
2021-03-08 09:45:29 +0800

    2-8 add personal configuration example--test.hello

--- wiki/src/main/java/com/chao/wiki/controller/TestController.java
+++ wiki/src/main/java/com/chao/wiki/controller/TestController.java
@@ -1,18 +1,19 @@
 package com.chao.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world!";
+        return "Hello world!" + testHello;
     }
 
     @PostMapping("/hello/post")
