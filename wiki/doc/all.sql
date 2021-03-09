a6cb79ce763fbd75b31bfb859b0232124a94dc0d
Thompson Wang <m18916275937@163.com>
2021-03-09 10:54:16 +0800

    3-5 integrate mybatis-generator and automatically generate 'Demo' table related code

---
+++ wiki/doc/all.sql
@@ -0,0 +1,17 @@
+create table `test` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `test` (id, name, password) values (1, '测试', 'password');
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名字',
+    primary key (`id`)
+) engine = innodb default charset = utf8mb4 comment = '测试';
+
+insert into `demo` (id, name) VALUES (1, '小红');
