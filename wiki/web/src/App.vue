a201c675b9d3bf2dfadda062e1d2b84ccf0260c0
Thompson Wang <m18916275937@163.com>
2021-03-11 19:34:55 +0800

    4-7 delete HelloWorld.vue and add the-footer.vue component

--- wiki/web/src/App.vue
+++ wiki/web/src/App.vue
@@ -2,20 +2,19 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      王超的电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
-    TheHeader,
+    TheHeader, TheFooter
   },
 });
 </script>
---
+++ wiki/web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    王超的电子书2
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
