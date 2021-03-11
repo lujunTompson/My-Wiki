f3a7ec6b3fca4364969f83d0cf891b32207751a9
Thompson Wang <m18916275937@163.com>
2021-03-11 20:58:58 +0800

    5-2 use axios to obtain data from API /ebook/list1

--- wiki/web/src/views/Home.vue
+++ wiki/web/src/views/Home.vue
@@ -55,9 +55,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-
+  setup() {
+    console.log("setup");
+    axios.get("http://localhost:8880/ebook/list1?name=mysql").then((response) => {
+      console.log(response);
+    })
+  }
 });
 </script>
