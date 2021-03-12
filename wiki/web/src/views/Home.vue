d50e4a14ac1cb46beb084c51bc2399caa60b9b8b
Thompson Wang <m18916275937@163.com>
2021-03-12 20:42:15 +0800

    5-4 integrate icons component

--- wiki/web/src/views/Home.vue
+++ wiki/web/src/views/Home.vue
@@ -48,21 +48,93 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ ebook }}
-{{ ebook2 }}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+
 import axios from 'axios';
 
+const listData: any = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
+  components: {
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
+
   name: 'Home',
   setup() {
+
+    const pagination = {
+      onChange: (page: any) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions = [
+      {
+        type: 'StarOutlined',
+        text: '156',
+      },
+      {
+        type: 'LikeOutlined',
+        text: '156',
+      },
+      {
+        type: 'MessageOutlined',
+        text: '2',
+      },
+    ];
+
     console.log("setup");
     const ebook = ref();
     const ebooks1 = reactive({books: []});
@@ -78,6 +150,11 @@ export default defineComponent({
     });
 
     return {
+
+      listData,
+      pagination,
+      actions,
+
       ebook,
       ebook2 : toRef(ebooks1, "books")
     }
