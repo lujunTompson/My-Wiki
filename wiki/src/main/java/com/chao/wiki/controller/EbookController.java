e0a1c742d721e09c9fb7457a918e61e63d622d7b
Thompson Wang <m18916275937@163.com>
2021-03-18 21:02:17 +0800

    6-11 integrate Validation and add parameter validation for list1 API

--- wiki/src/main/java/com/chao/wiki/controller/EbookController.java
+++ wiki/src/main/java/com/chao/wiki/controller/EbookController.java
@@ -9,6 +9,8 @@ import com.chao.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/ebook")
 public class EbookController {
@@ -18,7 +20,7 @@ public class EbookController {
 
     @GetMapping("/list1")
     //参数名字一样会自动映射
-    public CommonResp list(EbookQueryReq ebookQueryReq) {
+    public CommonResp list(@Valid EbookQueryReq ebookQueryReq) {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list = ebookService.list(ebookQueryReq);
         resp.setContent(list);
