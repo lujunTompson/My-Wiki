d17a8db36f587d9fd064324931701cbe7ead3424
Thompson Wang <m18916275937@163.com>
2021-03-20 12:52:31 +0800

    6-12 copy the object while editing, which can avoid impacting the data of the same line when we do not want to change it.

--- wiki/web/src/views/admin/admin-ebook.vue
+++ wiki/web/src/views/admin/admin-ebook.vue
@@ -85,6 +85,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import { message } from'ant-design-vue';
+import { Tool } from '@/util/tool';
 
 
 export default defineComponent({
@@ -202,7 +203,7 @@ export default defineComponent({
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = record;
+      ebook.value = Tool.copy(record);
     };
 
     /**
